6e0a49a996f8718c34aae906c6f67413aabce9c3
iLuzionsX <44279109+iLuzionsX@users.noreply.github.com>
2026-08-22 12:02:25 -0400

    Install hardened town runtime guards

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ import { installNatureInstancing } from './game/NatureInstancing.js';
 import { installMobileCameraControls } from './game/MobileCameraControls.js';
 import { installLumenwoodTown } from './game/TownSystem.js';
 import { clearLumenwoodFootprint } from './game/TownFootprint.js';
-import { installTownPerformanceGuard } from './game/TownPerformance.js';
+import { installTownRuntimeGuards } from './game/TownRuntime.js';
 
 const canvas = document.querySelector('#game');
 const game = new Game(canvas);
@@ -67,7 +67,7 @@ function waitForCoreVisuals(timeoutMs = 15000) {
 
 Promise.allSettled([waitForCoreVisuals(), environmentPromise, naturePromise]).then(async () => {
   clearLumenwoodFootprint(game);
-  town = installTownPerformanceGuard(installLumenwoodTown(game));
+  town = installTownRuntimeGuards(installLumenwoodTown(game));
   window.__MAPLES_TOWN__ = town;
   if (performancePass) {
     performancePass.rebatch();
